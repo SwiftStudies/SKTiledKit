06ba375e1d24efe4548086f5e9cfc7e1a4f3e180
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-09-21 18:37:50 -0500

    Changed object shape generation to leave the shape able to rotate about its center

--- Sources/SKTiledKit/SpriteKit/Level Processing/SKScene+ObjectLayer.swift
+++ Sources/SKTiledKit/SpriteKit/Level Processing/SKScene+ObjectLayer.swift
@@ -16,6 +16,60 @@ import SpriteKit
 import TiledKit
 
 extension SKScene{
+    
+    /// Creates a shape node for the supplied path
+    ///
+    /// - Parameters:
+    ///   - path: Path built in SpriteKit co-ordinate space
+    ///   - position: Position in Tiled co-ordinate space
+    ///   - degrees: The rotation in degrees (as specified in Tiled
+    ///   - centered: If true the shape will be positioned as it was in tiled, but the center of rotation will be the center of the shape
+    /// - Returns: The created node
+    func shapeNode(with path:CGPath, position:CGPoint, rotation degrees:Double, centered:Bool) -> SKTKShapeNode{
+        let position = position.transform()
+        let radians = -degrees.cgFloatValue.radians
+        
+        var path = path
+        //Rotate to create a new path
+        var rotationTransform = CGAffineTransform(rotationAngle: radians)
+
+        #warning("Force unwrap")
+        path = path.copy(using: &rotationTransform)!
+
+        if !centered {
+            let shapeNode = SKTKShapeNode(path: path)
+            shapeNode.position = position
+            return shapeNode
+        }
+        
+        var total = 0
+        var xSum : CGFloat = 0
+        var ySum : CGFloat = 0
+        
+        path.applyWithBlock { (pathElementPointer) in
+            
+            if !pathElementPointer.pointee.points.pointee.x.isNaN {
+                xSum += pathElementPointer.pointee.points.pointee.x
+                ySum += pathElementPointer.pointee.points.pointee.y
+                total += 1
+            }
+        }
+        
+        let center = CGPoint( x: xSum / total.cgFloatValue, y: ySum / total.cgFloatValue)
+        
+        var centeringTransform = CGAffineTransform(translationX: -center.x, y: -center.y)
+        
+        #warning("Force unwrap")
+        path = path.copy(using: &centeringTransform)!
+        
+        let shapeNode = SKTKShapeNode(path: path)
+        shapeNode.position = position
+        shapeNode.position.x += center.x
+        shapeNode.position.y += center.y
+        
+        return shapeNode
+    }
+    
     public func add(objects: ObjectLayer, to container: Container) throws {
         let objectLayerNode = SKNode()
         container.addChild(objectLayerNode)
@@ -33,12 +87,9 @@ extension SKScene{
             if let elipse = object as? EllipseObject {
                 let rect = CGRect(origin: .zero, size: CGSize(width: elipse.width, height: elipse.height).transform())
 
-                let elipseNode = SKTKShapeNode(path: CGPath(ellipseIn: rect, transform: nil))
+                let path = CGPath(ellipseIn: rect, transform: nil)
                 
-                elipseNode.position = CGPoint(x:elipse.x, y:elipse.y).transform()
-                elipseNode.zRotation = -elipse.rotation.radians.cgFloatValue
-                
-                objectNode = elipseNode
+                objectNode = shapeNode(with: path, position: CGPoint(x:elipse.x,y:elipse.y), rotation: elipse.rotation, centered: true)
             } else if let tileObject = object as? TileObject {
                 guard let tile = tileObject.level.tiles[tileObject.gid] else {
                     throw SKTiledKitError.tileNotFound
@@ -75,11 +126,10 @@ extension SKScene{
             } else if let rectangle = object as? RectangleObject {
                 let rect = CGRect(origin: .zero, size: CGSize(width: rectangle.width, height: rectangle.height).transform())
 
-                let rectangleNode = SKTKShapeNode(path: CGPath(rect: rect, transform: nil))
-                rectangleNode.position = CGPoint(x:rectangle.x, y:rectangle.y).transform()
-                rectangleNode.zRotation = -rectangle.rotation.radians.cgFloatValue
-
-                objectNode = rectangleNode
+                let path = CGPath(rect: rect, transform: nil)
+                
+                objectNode = shapeNode(with: path, position: CGPoint(x:rectangle.x,y:rectangle.y), rotation: rectangle.rotation, centered: true)
+                
             } else if let point = object as? PointObject {
                 let pointNode = SKTKShapeNode(circleOfRadius: 1)
                 
@@ -102,12 +152,7 @@ extension SKScene{
                     path.closeSubpath()
                 }
                 
-                let pathNode = SKTKShapeNode(path: path)
-
-                pathNode.position = CGPoint(x: polygon.x, y: polygon.y).transform()
-                pathNode.zRotation = -(polygon.rotation?.radians.cgFloatValue ?? 0)
-
-                objectNode = pathNode
+                objectNode = shapeNode(with: path, position: CGPoint(x: polygon.x, y: polygon.y), rotation: polygon.rotation, centered: true)
             }
             
             if let objectNode = objectNode {
