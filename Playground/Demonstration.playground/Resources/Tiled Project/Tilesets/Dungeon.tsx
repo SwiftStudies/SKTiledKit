027b710c76912af41a3dc09a422421717ddc85f8
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-09-29 09:12:59 -0500

    Updated playground Tiled project

--- Playground/Demonstration.playground/Resources/Tiled Project/Tilesets/Dungeon.tsx
+++ Playground/Demonstration.playground/Resources/Tiled Project/Tilesets/Dungeon.tsx
@@ -4,4 +4,35 @@
   <property name="filteringMode" value="nearest"/>
  </properties>
  <image source="../Images/DungeonTiles.png" width="256" height="256"/>
+ <tile id="152">
+  <animation>
+   <frame tileid="152" duration="100"/>
+   <frame tileid="153" duration="100"/>
+   <frame tileid="154" duration="100"/>
+   <frame tileid="155" duration="100"/>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="157" duration="100"/>
+   <frame tileid="158" duration="100"/>
+   <frame tileid="159" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="168">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="14">
+    <properties>
+     <property name="collisionMask" type="int" value="1"/>
+    </properties>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="168" duration="100"/>
+   <frame tileid="169" duration="100"/>
+   <frame tileid="170" duration="100"/>
+   <frame tileid="171" duration="100"/>
+   <frame tileid="172" duration="100"/>
+   <frame tileid="173" duration="100"/>
+   <frame tileid="174" duration="100"/>
+   <frame tileid="175" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
