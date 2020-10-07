08466133ee24a87e17ffe3509136d306f04511f4
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-07 12:56:26 -0500

    Properties for lights

---
+++ Sources/SKTiledKit/Map Processing/Properties/LightProperty.swift
@@ -0,0 +1,80 @@
+//    Copyright 2020 Swift Studies
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+
+import SpriteKit
+import TiledKit
+
+fileprivate extension SKLightNode {
+    var direction : Int {
+        get {
+            return Int(zRotation.degrees)
+        }
+        set {
+            zRotation = newValue.cgFloatValue.radians
+        }
+    }
+    
+    var lightCategory : Int {
+        get {
+            return Int(bitPattern: UInt(categoryBitMask))
+        }
+        set {
+            categoryBitMask = UInt32(bitPattern: Int32(newValue))
+        }
+    }
+}
+
+public enum LightProperty : String, AutomaticallyMappableProperty {
+    public typealias TargetObjectType = SKLightNode
+    
+    case lightCategory, ambientColor, lightColor, shadowColor, falloff, direction
+
+    public var keyPath: PartialKeyPath<SKLightNode> {
+        switch self {
+        case .lightCategory:
+            return \SKLightNode.lightCategory
+        case .ambientColor:
+            return \SKLightNode.ambientColor
+        case .lightColor:
+            return \SKLightNode.lightColor
+        case .shadowColor:
+            return \SKLightNode.shadowColor
+        case .falloff:
+            return \SKLightNode.falloff
+        case .direction:
+            return \SKLightNode.direction
+        }
+    }
+    
+    public var tiledPropertyName: String {
+        return rawValue
+    }
+    
+    public var tiledDefaultValue: PropertyValue {
+        switch self {
+        
+        case .lightCategory:
+            return .int(0)
+        case .ambientColor, .lightColor:
+            return .color(.white)
+        case .shadowColor:
+            return .color(.black)
+        case .falloff:
+            return 1.0
+        case .direction:
+            return 0
+        }
+    }
+}
