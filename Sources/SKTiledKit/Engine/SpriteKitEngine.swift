c7b52102d9a72397046e828eb6b9b2fb43a30677
Swift Studies <SwiftStudies@users.noreply.github.com>
2020-10-12 11:46:20 -0500

    Continuing to absorb improvements to TiledKit engine specialization... looking _really_ good

--- Sources/SKTiledKit/Engine/SpriteKitEngine.swift
+++ Sources/SKTiledKit/Engine/SpriteKitEngine.swift
@@ -16,7 +16,56 @@ import TiledKit
 import SpriteKit
 
 public class SpriteKitEngine : Engine {
+    public typealias MapType = SKScene
+    public typealias TextureType = SKTexture
+    public typealias SpriteType = SKSpriteNode
+    
     public typealias FloatType = CGFloat
     public typealias ColorType = SKColor
+    
+    public static func load(textureFrom url: URL, in project: Project) throws -> SKTexture {
+        return try project.retrieve(asType: SKTexture.self, from: url)
+    }
+    
+    public static func make(engineMapForTiled map: Map) throws -> SKScene {
+        return SKScene(size: map.pixelSize.cgSize)
+    }
+    
+    public static func postProcess(_ specializedMap: SKScene, for map: Map, from project: Project) throws -> SKScene {
+        return specializedMap
+    }
+    
+    public static func make(spriteFor tile: Tile, in tileset: TileSet, with texture: SKTexture, from project: Project) throws -> SKSpriteNode {
+        
+        #warning("Bounds need to be normalized")
+        let sprite = SKSpriteNode(texture: SKTexture(rect: tile.bounds.cgRect, in: texture))
+        
+        return sprite
+    }
+    
+    public static func postProcess(_ sprite: SKSpriteNode, from tile: Tile, in tileSet: TileSet, with setSprites: [UInt32 : SKSpriteNode], for map: Map, from project: Project) throws -> SKSpriteNode {
+        
+        #warning("Should be building animation frames etc")
+        return sprite
+    }
+}
+
+extension SKSpriteNode : EngineObject, DeepCopyable {
+    public typealias EngineType = SpriteKitEngine
+    
+    public func deepCopy() -> Self {
+        return copy() as! Self
+    }
+    
+    
+}
+
+extension SKTexture : EngineTexture {
+    public typealias EngineType = SpriteKitEngine
+}
+
+extension SKScene : EngineMap {
+    public typealias EngineType = SpriteKitEngine
+    
     
 }
